13fb2855fb1f86eb8d9c7ca317fd9079966dd782
Rony Ken <rm555149@fiap.com.br>
2023-04-17 09:54:33 -0300

    Usando o QuerySelector para puxar listas do html

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Recuperando o botão com o getElementById
+const botao = document.getElementById('btn-1');
+
+//Recuperar um item específico com a função QuerySelector(parâmetro)
+
+// const seletor = document.querySelector('li');
+// console.log(seletor.textContent);
+
+//Recuperar uma lista de elementos com a função QuerySelectorAll
+
+// const seletores = document.querySelectorAll('li');
+
+// seletores.forEach((item) =>{
+//     if (item.textContent == "Item-15") {
+//         let meuItem = item;
+//      // Usar sinal da crase e ${} é para melhorar a leitura do código
+//         console.log(`Items selecionado: ${meuItem.textContent}`);   
+//         meuItem.textContent = "Bom dia"
+//     }
+// })
+
+
+//Estudar:
+//setTiimeOut
+//setInterval
+//Random
+//Math.Ceil
+//Math.Random
+//Math.Floor
