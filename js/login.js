0403e90debc1fed0f3ee45446cfa36cbb9b5c93f
Rony Ken <rm551549@fiap.com.br>
2023-05-22 09:51:09 -0300

    redirect para pagina de sucesso

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 // não deixa o Javascript ficar varzeado, força a declaração das variaveis
 "user strict";
 
-
 // let usuario = "";
 // let senha = "";
 
@@ -26,8 +25,40 @@
 //         labelPass.setAttribute("style", "color:#ff0000;");
 //     }
 
-
 // })
+
+//LISTA DE USUÁRIOS NOVA
+let listaDeUsuarios = [
+
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Ruivão Lindão",
+        nomeUsuario : "ruivo",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Filipino de Deus",
+        nomeUsuario : "fili",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Pedro José",
+        nomeUsuario : "pepe",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Ricardito Nelson",
+        nomeUsuario : "rico",
+        senhaUsuario : "123456"
+    },
+]
+
+
+
 
 //Listener Global
 addEventListener("click", (evt)=> {
@@ -37,6 +68,12 @@ addEventListener("click", (evt)=> {
         
     const labelUser = document.querySelector("label[for='idUser']");
     const labelPass = document.querySelector("label[for='idPass']");
+
+    const msgStatus = document.querySelector("#msg");
+
+
+    //Criando o OBJETO USUARIO-VALIDADO
+    let usuarioValidado = {};
 
     //Recuperar o conte~udo dos campos e adicionar um OBJETO
     //CRIANDO O OBJETO USUARIO-LOGADO
@@ -48,8 +85,8 @@ addEventListener("click", (evt)=> {
     console.log(usuarioLogado.nomeUsuarioLogado);
 
 
+    
     if (evt.target.id == "btnSubmit") {
-        
         if(inputUser.value.length > 3 && inputPass.value.length > 3){
             // inputUser.setAttribute("style", "outline-color: #00ff00;")
             // inputPass.setAttribute("style", "outline-color: #00ff00;")
@@ -58,16 +95,35 @@ addEventListener("click", (evt)=> {
             labelPass.setAttribute("style", "color:#00ff00;");
 
             //Se preencheu a qntd de caractéres necessária agora vamos validar o nomeUsuario e a senhaUsuario
-            if (usuarioLogado.nomeUsuarioLogado == "tutu" && usuarioLogado.senhaUsuarioLogado == "123456") {
-                alert("VALIDADO!");
-            }else{
-                alert("NÃO VALIDADO");
-            }
 
-         }else{
+            //Chamando a lista de usuários e iterando para criar a validação para cada usuário contido nela.
+            listaDeUsuarios.forEach((usuario)=>{
+
+                if (usuarioLogado.nomeUsuarioLogado == usuario.nomeUsuario && usuarioLogado.senhaUsuarioLogado == usuario.senhaUsuario) {
+                    usuarioValidado = usuario;
+                }
+                
+            });
+            
+
+            if (usuarioValidado.nomeUsuario != undefined) {
+                msgStatus.setAttribute("style","color:#00ff00");
+                msgStatus.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} realizou o login efetuado com Sucesso!</strong></span>`;
+
+                //Redirect
+
+                setTimeout(()=>{
+                    window.location.href = "../sucesso.html"
+                }, 3000);
+
+            }else{
+                msgStatus.setAttribute("style","color:#ff0000");
+                msgStatus.innerHTML = "<span><strong>Login ou senha incorretos!</strong></span>"
+            }
+            
+        }else{
             labelUser.setAttribute("style", "color:#ff0000;");
             labelPass.setAttribute("style", "color:#ff0000;");
         }
-    
     }
 })
